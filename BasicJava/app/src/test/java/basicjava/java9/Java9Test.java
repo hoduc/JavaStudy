4fe05b04f501601dc7d6265cd51b5cca18ef0b1a
hoduc <duc_ho90@yahoo.com>
2026-05-01 10:33:14 -0700

    [BasicJava][Java9] ProcessInfo other info

--- BasicJava/app/src/test/java/basicjava/java9/Java9Test.java
+++ BasicJava/app/src/test/java/basicjava/java9/Java9Test.java
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.Test;
 import basicjava.java9.Java9.EchoProcess;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Map;
 
 public class Java9Test {
@@ -18,8 +14,11 @@ public class Java9Test {
         Map.Entry<Process, Integer> processExit = echoProcess.echo("hello", "world");
         Process process = processExit.getKey();
         if (process != null) {
-            ProcessHandle.Info processInfo = process.toHandle().info();
+            ProcessHandle processHandle = process.toHandle();
+            ProcessHandle.Info processInfo = processHandle.info();
+            assertFalse(processHandle.isAlive());
             assertTrue(processInfo.totalCpuDuration().isPresent());
+            assertTrue(processInfo.user().isPresent());
         }
 
     }
