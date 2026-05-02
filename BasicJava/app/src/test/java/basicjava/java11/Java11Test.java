d427fbd405a690ad501b63adcbcdb93b51c39e4d
hoduc <duc_ho90@yahoo.com>
2026-05-02 16:54:55 -0700

    [BasicJava][Java11] var on lambda expression: https://openjdk.org/jeps/323

---
+++ BasicJava/app/src/test/java/basicjava/java11/Java11Test.java
@@ -0,0 +1,25 @@
+package basicjava.java11;
+
+import org.jspecify.annotations.NonNull;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Java11Test {
+    @Test
+    void localVariableTypeInferenceOnLambdaExpression() {
+        
+        assertEquals(
+            List.of("A", "B", "C"),
+            List.of("a", "b", "c").stream()
+            .map((@NonNull var s) -> s.toUpperCase())
+            .toList()
+        );
+    }
+}
