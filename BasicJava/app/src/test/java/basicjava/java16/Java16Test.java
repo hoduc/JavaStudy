ced89c3a7ee6810b10ef0034a2e71154439fe77c
hoduc <duc_ho90@yahoo.com>
2026-05-03 11:52:07 -0700

    [BasicJava][Java16] record https://openjdk.org/jeps/395

--- BasicJava/app/src/test/java/basicjava/java16/Java16Test.java
+++ BasicJava/app/src/test/java/basicjava/java16/Java16Test.java
@@ -1,11 +1,18 @@
 package basicjava.java16;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class Java16Test {
 
+    record Range(int lo, int hi) {
+        Range {
+            if (lo > hi)
+                throw new IllegalArgumentException(String.format("(%d,%d)", lo, hi));
+        }
+    }
     private static String runtimeType(Object obj) {
         String t = "unknown";
         // switch does not do pattern matching yet until java 21
@@ -22,6 +29,27 @@ public class Java16Test {
         assertEquals("[hello]:string", runtimeType("hello"));
         assertEquals("[42]:int", runtimeType(42));
         assertEquals("unknown", runtimeType(42.0));
+    }
+
+    @Test
+    void record() {
+        // example lifted from: https://openjdk.org/jeps/395
+        record LocalRange(int lo, int hi) {
+            LocalRange {
+                if (lo > hi)
+                    throw new IllegalArgumentException(String.format("(%d,%d)", lo, hi));
+            }
+        }
+
+        var lr = new LocalRange(1, 10);
+        assertEquals(1, lr.lo());
+        assertEquals(10, lr.hi());
+
+        assertThrows(IllegalArgumentException.class, () -> new LocalRange(10, 1));
+
+        var r = new Range(1, 10);
+        assertEquals(1, r.lo());
+        assertEquals(10, r.hi());
     }
     
 }
